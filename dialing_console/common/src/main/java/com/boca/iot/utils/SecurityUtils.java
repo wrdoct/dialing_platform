03f5fc0b4f2f865a5e4f407df3503a34b36a435a
llw <lulinwei@inspur.com>
2024-04-10 14:41:24 +0800

    xx

---
+++ dialing_console/common/src/main/java/com/boca/iot/utils/SecurityUtils.java
@@ -0,0 +1,92 @@
+//package com.boca.iot.utils;
+//
+//import org.apache.shiro.subject.Subject;
+//
+///**
+// * @author mhma
+// */
+//public class SecurityUtils {
+//
+//    public static final String REQUEST_AUTH_HEADER = "Authorization";
+//    public static final String APP_AUTH_HEADER = "token";
+//    public static final String TENANT_ID_COLUMN = "developer_id";
+//    public static final String TENANT = "tenant";
+//    public static final String ADMIN_TENANT_ID = "-1";
+//
+//    public static Long getCurrentUserId() {
+//        Subject currentUser = org.apache.shiro.SecurityUtils.getSubject();
+//        return JwtUtils.getUserId(currentUser.getPrincipal().toString());
+//    }
+//
+//    public static String getCurrentUserAccount() {
+//        Subject currentUser = org.apache.shiro.SecurityUtils.getSubject();
+//        return JwtUtils.getUserAccount(currentUser.getPrincipal().toString());
+//    }
+//
+//    public static Subject getSubject() {
+//        return org.apache.shiro.SecurityUtils.getSubject();
+//    }
+//
+//    public static boolean hasRole(String roleName) {
+//        return getSubject() != null && roleName != null && roleName.length() > 0 && getSubject().hasRole(roleName);
+//    }
+//
+//    public static boolean lacksRole(String roleName) {
+//        return !hasRole(roleName);
+//    }
+//
+//    public static boolean hasAnyRoles(String roleNames) {
+//        boolean hasAnyRole = false;
+//        Subject subject = getSubject();
+//        if (subject != null && roleNames != null && roleNames.length() > 0) {
+//            String[] var3 = roleNames.split(",");
+//            int var4 = var3.length;
+//
+//            for(int var5 = 0; var5 < var4; ++var5) {
+//                String role = var3[var5];
+//                if (subject.hasRole(role.trim())) {
+//                    hasAnyRole = true;
+//                    break;
+//                }
+//            }
+//        }
+//
+//        return hasAnyRole;
+//    }
+//
+//    public static boolean hasAllRoles(String roleNames) {
+//        boolean hasAllRole = true;
+//        Subject subject = getSubject();
+//        if (subject != null && roleNames != null && roleNames.length() > 0) {
+//            String[] var3 = roleNames.split(",");
+//            int var4 = var3.length;
+//
+//            for(int var5 = 0; var5 < var4; ++var5) {
+//                String role = var3[var5];
+//                if (!subject.hasRole(role.trim())) {
+//                    hasAllRole = false;
+//                    break;
+//                }
+//            }
+//        }
+//
+//        return hasAllRole;
+//    }
+//
+//    public static boolean hasPermission(String permission) {
+//        return getSubject() != null && permission != null && permission.length() > 0 && getSubject().isPermitted(permission);
+//    }
+//
+//    public static boolean lacksPermission(String permission) {
+//        return !hasPermission(permission);
+//    }
+//
+//    public static String principal() {
+//        if (getSubject() != null) {
+//            Object principal = getSubject().getPrincipal();
+//            return principal.toString();
+//        } else {
+//            return "";
+//        }
+//    }
+//}
---
+++ dialing_console/manager/src/main/java/com/boca/iot/config/ShiroConfig.java
@@ -0,0 +1,106 @@
+//package com.boca.iot.config;
+//
+//import org.apache.shiro.mgt.DefaultSessionStorageEvaluator;
+//import org.apache.shiro.mgt.DefaultSubjectDAO;
+//import org.apache.shiro.mgt.SecurityManager;
+//import org.apache.shiro.spring.LifecycleBeanPostProcessor;
+//import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
+//import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+//import org.apache.shiro.web.filter.authz.SslFilter;
+//import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+//import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
+//import org.springframework.beans.factory.annotation.Qualifier;
+//import org.springframework.boot.web.servlet.FilterRegistrationBean;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.filter.DelegatingFilterProxy;
+//
+//import javax.servlet.Filter;
+//import java.util.HashMap;
+//import java.util.LinkedHashMap;
+//import java.util.Map;
+//
+///**
+// * @author llw
+// */
+//@Configuration
+//public class ShiroConfig {
+//
+//    @Bean(name = "shiroFilter")
+//    public ShiroFilterFactoryBean shiroFilter(@Qualifier("securityManager") SecurityManager manager) {
+//        ShiroFilterFactoryBean shiroFilter = new ShiroFilterFactoryBean();
+//        Map<String, Filter> filters = new HashMap<>(2);
+//        filters.put("ssl", new SslFilter());
+//        filters.put("jwt", new JwtFilter());
+//        shiroFilter.setFilters(filters);
+//        shiroFilter.setLoginUrl("/sec/login");
+//        shiroFilter.setSecurityManager(manager);
+//        shiroFilter.setUnauthorizedUrl("/sec/401");
+//
+//        // 配置访问权限
+//        LinkedHashMap<String, String> filterRules = new LinkedHashMap<>();
+//        filterRules.put("/webjars/**", "anon");
+//        filterRules.put("/swagger**", "anon");
+//        filterRules.put("/v2/api-docs", "anon");
+//        filterRules.put("/swagger-resources/configuration/ui", "anon");
+//        filterRules.put("/doc.html", "anon");
+//
+//        filterRules.put("/**", "jwt");
+//        shiroFilter.setFilterChainDefinitionMap(filterRules);
+//        return shiroFilter;
+//    }
+//
+//    /**
+//     * 配置核心安全事务管理器
+//     */
+//    @Bean(name = "securityManager")
+//    public SecurityManager securityManager() {
+//        DefaultWebSecurityManager manager = new DefaultWebSecurityManager();
+//        manager.setRealm(jwtRealm());
+//        // 关闭shiro默认的sessionDao
+//        DefaultSubjectDAO subjectDAO = new DefaultSubjectDAO();
+//        DefaultSessionStorageEvaluator defaultSessionStorageEvaluator = new DefaultSessionStorageEvaluator();
+//        defaultSessionStorageEvaluator.setSessionStorageEnabled(false);
+//        subjectDAO.setSessionStorageEvaluator(defaultSessionStorageEvaluator);
+//        manager.setSubjectDAO(subjectDAO);
+//        return manager;
+//    }
+//
+//    /**
+//     * 配置自定义的权限登录器
+//     */
+//    @Bean(name = "jwtRealm")
+//    public JwtRealm jwtRealm() {
+//        JwtRealm jwtRealm = new JwtRealm();
+//        return jwtRealm;
+//    }
+//
+//    @Bean
+//    public FilterRegistrationBean filterRegistrationBean() {
+//        FilterRegistrationBean filterRegistration = new FilterRegistrationBean();
+//        filterRegistration.setFilter(new DelegatingFilterProxy("shiroFilter"));
+//        filterRegistration.setEnabled(true);
+//        filterRegistration.setOrder(1);
+//        filterRegistration.addUrlPatterns("/*");
+//        return filterRegistration;
+//    }
+//
+//    @Bean
+//    public LifecycleBeanPostProcessor lifecycleBeanPostProcessor() {
+//        return new LifecycleBeanPostProcessor();
+//    }
+//
+//    @Bean
+//    public DefaultAdvisorAutoProxyCreator defaultAdvisorAutoProxyCreator() {
+//        DefaultAdvisorAutoProxyCreator creator = new DefaultAdvisorAutoProxyCreator();
+//        creator.setProxyTargetClass(true);
+//        return creator;
+//    }
+//
+//    @Bean
+//    public AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor(@Qualifier("securityManager") SecurityManager manager) {
+//        AuthorizationAttributeSourceAdvisor advisor = new AuthorizationAttributeSourceAdvisor();
+//        advisor.setSecurityManager(manager);
+//        return advisor;
+//    }
+//}
